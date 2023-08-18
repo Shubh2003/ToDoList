5e0b0c77130a987d3bc101d0865b204f0fc1e3bd
Shubham Daulkar <daulkarshubham@gmail.com>
2023-08-19 02:17:21 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Admin on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
